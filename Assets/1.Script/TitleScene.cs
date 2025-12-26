a06d59ded1a2ecdd1d09fb6d61b74cbb2d879555
PoPo <dltngus7535@gmail.com>
2025-12-26 10:22:59 +0900

    폰트추가 및 버튼어트리트뷰

--- Assets/1.Script/TitleScene.cs
+++ Assets/1.Script/TitleScene.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Linq;
 using UnityEngine;
-public class TitleScene : MonoBehaviour
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScene : SetInspectorMonoBehaviour
 {
-    [Button]
-    void Test()
+    public Button startBtn;
+    public override void SetInspector()
     {
-        "qefg".Log();
+        var child = transform.GetAllChild();
+        startBtn = child.First(obj=> obj.name == "Start Btn").GetComponent<Button>();
     }
 
+    public void Start()
+    {
+        startBtn.onClick.AddListener(()=>
+        {
+            SceneManager.LoadScene("GameScene");
+        });
+    }
 }
-
-
 
 
